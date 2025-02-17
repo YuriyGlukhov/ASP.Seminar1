6c4ce35d50763f9609d1b580207f8cc4e79dbe63
Your Name <you@example.com>
2025-02-17 18:44:40 +0300

    Add project files.

---
+++ ASP.Seminar1/Controllers/CategoryController.cs
@@ -0,0 +1,97 @@
+﻿using ASP.Seminar1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.Seminar1.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet("getCategory")]
+        public ActionResult<IEnumerable<Category>> GetCategory()
+        {
+            try
+            {
+                using (var context = new ProductContext())
+                {
+                    var categories = context.Categories.Select(x => new Category()
+                    {
+                        Name = x.Name,
+                        Id = x.Id,
+                        Description = x.Description
+                    }).ToList();
+                    return categories;
+                }
+
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+
+
+        }
+
+        [HttpPost("postCategory")]
+        public IActionResult PostCategory([FromQuery] string name, string description)
+        {
+            try
+            {
+                using (var context = new ProductContext())
+                {
+                    if (context.Categories.Any(x => x.Name.ToLower() == name.ToLower()))
+                    {
+                        return StatusCode(409);
+                    }
+                    else
+                    {
+                        context.Categories.Add(new Category()
+                        {
+                            Name = name,
+                            Description = description
+
+                        });
+                        context.SaveChanges();
+
+                        return Ok();
+                    }
+                }
+
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+
+        [HttpDelete("deleteCategory")]
+        public IActionResult DeleteCategory(string name)
+        {
+            try
+            {
+                using (var context = new ProductContext())
+                {
+                    if (context.Categories.Any(x => x.Name.ToLower() == name.ToLower()))
+                    {
+                        var category = context.Categories.FirstOrDefault(x => x.Name.ToLower() == name.ToLower());
+                        context.Categories.Remove(category);
+                        context.SaveChanges();
+
+                        return Ok();
+                    }
+                    else
+                    {
+                        return StatusCode(409);
+                    }
+                }
+
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
---
+++ ASP.Seminar1/Controllers/ProductController.cs
@@ -0,0 +1,99 @@
+﻿using ASP.Seminar1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.Seminar1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet("getProduct")]
+        public ActionResult<IEnumerable<Product>> GetProducts()
+        {
+            try
+            {
+                using (var context = new ProductContext())
+                {
+                    var products = context.Products.Select(x => new Product() 
+                    {
+                        Name = x.Name,
+                        Id = x.Id,
+                        Description = x.Description,
+                        Category = x.Category,
+                        Cost = x.Cost,
+                        CategoryId = x.CategoryId,
+                    }).ToList();
+                    return products;
+                }
+
+            }
+            catch 
+            {
+                return StatusCode(500);
+            } 
+        }
+
+        [HttpPost("postProduct")]
+        public IActionResult PostProduct(string name, string description, int price, int groupId)
+        {
+            try
+            {
+                using (var context = new ProductContext())
+                {
+                    if (!context.Products.Any(x => x.Name.ToLower() == name.ToLower()))
+                    {
+                        context.Products.Add(new Product()
+                        {
+                            Name = name,
+                            Description = description,
+                            Cost = price,
+                            CategoryId = groupId
+                        });
+                        context.SaveChanges();
+
+                        return Ok();
+                    }
+                    else
+                    {
+
+                       return StatusCode(409);
+                    }
+                }
+
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+
+        [HttpDelete("deleteProduct")]
+        public IActionResult DeleteProduct(string name)
+        {
+            try
+            {
+                using (var context = new ProductContext())
+                {
+                    if (context.Products.Any(x => x.Name.ToLower() == name.ToLower()))
+                    {
+                        var product = context.Products.FirstOrDefault(x =>x.Name.ToLower() == name.ToLower());
+                        context.Products.Remove(product);
+                        context.SaveChanges(); 
+                        
+                        return Ok();
+                    }
+                    else
+                    {
+                        return StatusCode(409);
+                    }
+                }
+
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
